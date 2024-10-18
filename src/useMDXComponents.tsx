aba416e7087d53db041ea19bd25c2c523a7657e6
Pakorn Nathong <pakorn.nth@hotmail.com>
2024-10-18 14:04:01 +0700

    feat: add table components

--- src/useMDXComponents.tsx
+++ src/useMDXComponents.tsx
@@ -2,11 +2,40 @@ import Image, { ImageProps } from "next/image"
 import type { MDXComponents } from "mdx/types"
 
 import { Separator } from "./components/ui/separator"
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "./components/ui/table"
+import { cn } from "./lib/utils"
 
 export const MDX_COMPONENTS: MDXComponents = {
   hr: (props) => <Separator {...props} className="my-8" />,
   blockquote: (props) => (
     <blockquote {...props} className="my-4 border-l-4 border-border pl-4" />
+  ),
+  table: (props) => (
+    <div className="rounded-md border">
+      <Table {...props} className={cn("my-0", props.className)} />
+    </div>
+  ),
+  tr: (props) => (
+    <TableRow {...props} className={cn("border-border", props.className)} />
+  ),
+  thead: (props) => <TableHeader {...props} />,
+  td: (props) => <TableCell {...props} />,
+  th: (props) => (
+    <TableHead {...props} className={cn("py-0", props.className)} />
+  ),
+  tbody: (props) => <TableBody {...props} />,
+  tfoot: (props) => <TableFooter {...props} />,
+  caption: (props) => (
+    <TableCaption className={props.className} children={props.children} />
   ),
 } as const
 
