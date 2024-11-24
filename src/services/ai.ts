39d565f5d983f1ce44eb8f7f1694502c9ef1e99f
Pakorn Nathong <pakorn.nth@hotmail.com>
2024-11-24 19:43:56 +0700

    feat: add zk-ninja header

--- src/services/ai.ts
+++ src/services/ai.ts
@@ -5,6 +5,10 @@ import { env } from "@/env.mjs"
 export const provider = createOpenAI({
   baseURL: env.AI.ENDPOINT,
   apiKey: env.AI.API_KEY,
+  headers: {
+    "HTTP-Referer": env.NEXT_PUBLIC_APP_URL,
+    "X-Title": "zk.ninja",
+  }
 })
 
 export const model = provider(env.AI.MODEL_NAME)
